e2a96b5754d162b9f049be414077739598af348d
ykursav <emrekursav91@hotmail.com>
2022-02-22 21:43:50 +0100

    Completed clock implementation. Not bad :)

---
+++ src/realtime_clock.js
@@ -0,0 +1,19 @@
+
+function getRealDate(){
+    const currentDate = new Date();
+    
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    let amPm = hours >= 12 ? 'pm' : 'am';
+    
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    let strTime = `${String(hours).padStart(2, 0)}:${String(minutes).padStart(2, 0)}:${String(seconds).padStart(2,0)} ${amPm}`;
+    document.getElementById('clock').innerText = strTime;
+    clock.innerHTML = strTime;
+    setTimeout(getRealDate, 1000);
+}
+
+
+getRealDate()
